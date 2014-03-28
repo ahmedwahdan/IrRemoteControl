a19b0fec3d5369278b0b3ef05fea3291df33b349
ahmedwahdan <ahmedibrahimwahdan@gmail.com>
2014-03-28 15:11:11 +0200

    new project created

---
+++ IrRemoteControl/IrRemoteControl.c
@@ -0,0 +1,17 @@
+﻿/*
+ * IrRemoteControl.c
+ *
+ * Created: 28/03/2014 03:00:06 م
+ *  Author: master
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
